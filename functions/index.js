b76f9f0e8f0e4f41d4725ce5f70eae664e557470
fiksie <fiksie@gmail.com>
2022-11-30 17:40:26 +0000

    secfix

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-var serviceAccount = require("./permissions.json");
+var serviceAccount = require("permission");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
